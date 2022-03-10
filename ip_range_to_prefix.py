0d80f6ed6aab39bb45ef3961f5c2fa51feee42a9
Maaz <62890173+maazshakeel@users.noreply.github.com>
2022-03-11 01:39:05 +0500

    Commented the code!

--- ip_range_to_prefix.py
+++ ip_range_to_prefix.py
@@ -17,35 +17,32 @@ with open(sys.argv[1], 'r') as file:
     # reading the lines in the file
     file_data = file.readlines()
 
-for i in range(len(file_data)):
-    net_addr = file_data[i].split('-')[0]
-    broadcast_addr = file_data[i].split('-')[1]
+    # looping through
+    for i in range(len(file_data)):
+        net_addr = file_data[i].split('-')[0]
+        broadcast_addr = file_data[i].split('-')[1]
 
-    net_id_octects = net_addr.split('.')
-    broad_id_octects = broadcast_addr.split('.')
+        net_id_octects = net_addr.split('.')
+        broad_id_octects = broadcast_addr.split('.')
 
-    same_octect = 0
-    same_bit = 0
+        same_octect = 0
+        same_bit = 0
 
-    # prefix length - net mask convertion
-    for k in range(4):
-        net_id_octects[k] = int(net_id_octects[k])
-        broad_id_octects[k] = int(broad_id_octects[k])
-        if net_id_octects[k] == broad_id_octects[k]:
-            same_octect += 1
-        else:
-            net_one = format(net_id_octects[k], '08b') 
-            broad_one = format(broad_id_octects[k], '08b')
-            for j in range(8):
-                if net_one[j] == broad_one[j]:
-                    same_bit += 1
-            
-            
+        # prefix length - net mask convertion
+        for k in range(4):
+            net_id_octects[k] = int(net_id_octects[k])
+            broad_id_octects[k] = int(broad_id_octects[k])
+            if net_id_octects[k] == broad_id_octects[k]:
+                same_octect += 1
+            else:
+                net_one = format(net_id_octects[k], '08b') 
+                broad_one = format(broad_id_octects[k], '08b')
+                for j in range(8):
+                    if net_one[j] == broad_one[j]:
+                        same_bit += 1
+                
+                
 
-    # prefix
     # saving the result in the new file
     with open('prefix_result.txt', 'a') as file:
         file.write(f"{net_addr}/{((same_octect)*8) + same_bit}\n")
-
-
-# A change checkout this change
